2bc0ebfcd9cd9c72bb0807f86cfb0821a684cb64
Darsh Jain <144017031+darshjnn@users.noreply.github.com>
2023-10-17 23:28:25 +0530

    Add files via upload

---
+++ UsernameFromEmail.java
@@ -0,0 +1,23 @@
+package Strings;
+
+import java.util.*;
+
+public class UsernameFromEmail {
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            String inp, userName = "";
+            int p, i;
+            System.out.print("Enter Email-id: ");
+            inp = sc.nextLine();
+
+            p = inp.indexOf('@');
+
+            for(i=0; i<p; i++){
+                userName += inp.charAt(i);
+            }
+
+            System.out.print("Your User Name is: " + userName);
+            
+        }
+    }
+}
