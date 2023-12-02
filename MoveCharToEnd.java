314a6b0a2439324848f5b922a3e79e24a514846d
darshjnn <darshjain12345654321@gmail.com>
2023-12-02 15:21:50 +0530

    Added Move any Character to end

--- MoveCharToEnd.java
+++ MoveCharToEnd.java
@@ -3,8 +3,30 @@
 
 public class MoveCharToEnd {
 
+    public static int count = 0; 
+
+    public static String moveString(String str, char ele, int index, int count){
+        if (index == str.length()){
+            return addEle(count);
+        }
+
+        if (str.charAt(index) == ele){
+            return moveString(str, ele, index + 1, count + 1);
+        } else {
+            return str.charAt(index) + moveString(str, ele, index + 1, count);
+        }
+    }
+
+    public static String addEle (int count){
+        String newStr = "x";
+        for (int i = 0; i <= count; i++){
+            newStr += "x";
+        }
+        return newStr;
+    } 
     public static void main(String args[]) {
-        String str = "xyjjdkxdxljka";
+        String str = "xxyjjdkxdxljka";
         char el = 'x';
+        System.out.println(moveString(str, el, 0, 0));
     }
 }
