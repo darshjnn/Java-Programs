0fd67095bde9052b4e55c67d8b2bf8a4bd6b4f6a
darshjnn <darshjain12345654321@gmail.com>
2024-02-25 12:19:42 +0530

    Added More Programs to LeetCode

--- LeetCode/DeleteNthNodeFromLast.java
+++ LeetCode/DeleteNthNodeFromLast.java
@@ -1,5 +1,32 @@
-package SingleLinkedList;
-//Delete nth Node from the last
+/*
+Remove Nth Node From End of List
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+
+*/
+
+/*
+Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+*/
 
 public class DeleteNthNodeFromLast {
     public class ListNode {
--- LeetCode/SwapNodes.java
+++ LeetCode/SwapNodes.java
@@ -1,34 +1,59 @@
-package SingleLinkedList;
+/*
+Swap Nodes in Pairs
 
-//  Definition for singly-linked list.
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
-class SwapPairs {
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+Input: head = []
+Output: []
+
+Example 3:
+Input: head = [1]
+Output: [1]
+
+*/
+
+// Definition for singly-linked list.
+class SwapNodes {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
-    
+
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        
+
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prevNode = dummy;
         ListNode currNode = head;
-        
+
         while (currNode != null && currNode.next != null) {
             ListNode nextNode = currNode.next.next;
             ListNode second = currNode.next;
-            
+
             currNode.next = nextNode;
             second.next = currNode;
             prevNode.next = second;
-            
+
             prevNode = currNode;
             currNode = nextNode;
         }
